69cdc7fcf4901c1070030a95d15453047e47bace
testcocoapod <nguyenphu2810.2@gmail.com>
2020-12-28 17:49:28 +0700

    update code

--- App.js
+++ App.js
@@ -6,29 +6,17 @@
  * @flow strict-local
  */
 
-import React, {useEffect, useState, useCallback} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React, {useEffect, useCallback} from 'react';
+import {View, Button} from 'react-native';
 import RNZoomView from 'react-native-zoom-us';
-import { NativeModules } from 'react-native';
-const { ZoomModule } = NativeModules;
+import {NativeModules, NativeEventEmitter} from 'react-native';
+const {ZoomModule} = NativeModules;
+const eventEmitter = new NativeEventEmitter(ZoomModule);
 
 const App: () => React$Node = () => {
+  const handleEventMeeting = useCallback((event) => {
+    console.log(event);
+  }, []);
 
   useEffect(() => {
     ZoomModule.initZoomSDK({
@@ -36,7 +24,16 @@ const App: () => React$Node = () => {
       clientKey: 'yaEkS5rguwHNuFvqOsDh8VMvZOkRSNEMJpjn',
       clientSecret: 'ngtmOzHAu0FwI55Faoe0AD3tVm86D3XfkzTj',
     });
-  }, []);
+    const subscriptionEvent = eventEmitter.addListener(
+      'onMeetingEvent',
+      handleEventMeeting,
+    );
+    ZoomModule.startObserverEvent();
+    return () => {
+      subscriptionEvent.remove();
+      ZoomModule.stopObserverEvent();
+    };
+  }, [handleEventMeeting]);
 
   const leaveMeeting = useCallback(() => {
     ZoomModule.leaveCurrentMeeting();
@@ -44,8 +41,8 @@ const App: () => React$Node = () => {
 
   const joinMeeting = useCallback(() => {
     ZoomModule.joinMeeting({
-      roomNumber: '72308688384',
-      roomPassword: '2WXXGY',
+      roomNumber: '75348310869',
+      roomPassword: 'N7B5Zi',
       userDisplayName: 'Phu2',
       userPassword: '123',
     });
@@ -67,80 +64,37 @@ const App: () => React$Node = () => {
   }, []);
   return (
     <View style={{flex: 1, backgroundColor: 'blue'}}>
-      <RNZoomView style={{flex: 1}} />
-      <View style={{marginBottom: 40, backgroundColor: 'green', flexDirection: 'row', flexWrap: 'wrap'}}>
+      <RNZoomView style={{flex: 1}} userID={'active_user'} />
+      <RNZoomView
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          width: 100,
+          height: 150,
+        }}
+        userID={'local_user'}
+      />
+      <View
+        style={{
+          marginBottom: 40,
+          backgroundColor: 'green',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}>
+        <Button onPress={joinMeeting} title=" Join " color="#841584" />
+        <Button onPress={leaveMeeting} title=" Leave " color="#841584" />
+        <Button onPress={onOffAudio} title=" On/Off audio " color="#841584" />
+        <Button onPress={onOffVideo} title=" On/Off video " color="#841584" />
         <Button
-            onPress={joinMeeting}
-            title=" Join "
-            color="#841584"
+          onPress={switchCamera}
+          title=" Switch Camera "
+          color="#841584"
         />
-        <Button
-            onPress={leaveMeeting}
-            title=" Leave "
-            color="#841584"
-        />
-        <Button
-            onPress={onOffAudio}
-            title=" On/Off audio "
-            color="#841584"
-        />
-        <Button
-            onPress={onOffVideo}
-            title=" On/Off video "
-            color="#841584"
-        />
-        <Button
-            onPress={switchCamera}
-            title=" Switch Camera "
-            color="#841584"
-        />
-        <Button
-            onPress={getParticipants}
-            title=" Members "
-            color="#841584"
-        />
+        <Button onPress={getParticipants} title=" Members " color="#841584" />
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
